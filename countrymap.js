b27984e6698cbee599c5512aa85ef780972f5541
saarthakrana <91086362+saarthakrana@users.noreply.github.com>
2023-09-21 10:02:04 +0530

    countrymap.js

--- countrymap.js
+++ countrymap.js
@@ -250,7 +250,7 @@ Use pursuant to license agreement at https://simplemaps.com/license */
           "font-weight": 400,
           gradient: 0,
           height: 0,
-          href: "http://raphaeljs.com/",
+          href: "",
           "letter-spacing": 0,
           opacity: 1,
           path: "M0,0",
@@ -7142,9 +7142,6 @@ var simplemaps_countrymap_mapinfo = {
           e +
           " !important; z-index: 1 !important; position: absolute !important; filter: opacity(1) !important;"),
           e8.appendChild(tI);
-          // (tI.innerHTML =
-          //   '<a style="overflow: visible !important; clip-path: none !important; opacity: 1 !important;  filter: opacity(1) !important; transform: none !important; display: block !important; visibility: visible !important; font: 18px Verdana, Arial, Helvetica, sans-serif !important; cursor: pointer !important; font-weight: bold !important; float: right !important; color: #000000 !important; text-decoration: none !important;" href="https://simplemaps.com" title="For evaluation use only.">Simplemaps.com Trial</a>'),
-          // !ea && 503 != tI.innerHTML.length && (e8.innerHTML = "");
       }
     }
     function tj() {
